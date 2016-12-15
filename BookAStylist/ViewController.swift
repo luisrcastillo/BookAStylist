05a4bc77f2f33f20713a509c20fc198d97027346
Luis Castillo <luisrcastillo1@gmail.com>
2016-12-15 09:25:34 -0500

    Initial Commit

---
+++ BookAStylist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookAStylist
+//
+//  Created by Luis Castillo on 12/15/16.
+//  Copyright © 2016 Castle Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
